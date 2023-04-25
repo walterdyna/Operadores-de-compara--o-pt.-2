719d1344ff5df04b6e1a08ff42d01d3175734d73
walterdyna <alexdyna@gmail.com>
2023-04-25 14:36:59 -0300

    Maior ou menor

---
+++ Operadores de comparação  pt2.js
@@ -0,0 +1,23 @@
+// Operadores de comparação parte 2
+
+
+// >   -> Maior
+// >=  -> Maior ou Igual
+// <   -> Menor
+// <=  -> Menor ou Igual
+
+// Operadores de comparação  maior ou menos ou iguel...
+
+const firstNumber = 12
+const seccondNumber = 10
+
+// ---------------Maior-----------------
+console.log(firstNumber > seccondNumber)
+
+// ---------- Maior ou Igual -----------
+console.log(firstNumber>= seccondNumber)
+
+// -------------- Menor ----------------
+console.log(firstNumber < seccondNumber)
+// ---------- Manor ou Igual -----------
+console.log(firstNumber <= seccondNumber)
---
+++ README.md
@@ -0,0 +1,15 @@
+# Operadores-de-Comparação - JavaScript
+
+
+// ---------------------------- ===== Operadores de comparação ===== --------------------------------
+
+
+// Operadores de comparação parte 2 ----Utilizado para comparar um valor a outro
+
+
+// >   -> Maior
+// >=  -> Maior ou Igual
+// <   -> Menor
+// <=  -> Menor ou Igual
+
+// Operadores de comparação  maior ou menos ou igual...
